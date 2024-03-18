e449103789df6941afcb39840cde0a6e007c1170
Yori <yusufmj605@gmail.com>
2024-03-18 17:29:50 +0300

    my wsl doesn't work

--- Functions/initialconn/startserver.go
+++ Functions/initialconn/startserver.go
@@ -12,16 +12,17 @@ import (
 // This project consists on recreating the NetCat in a Server-Client Architecture that can run in a server mode on a specified port listening for incoming connections, and it can be used in client mode, trying to connect to a specified port and transmitting information to the server.
 
 func StartServer(ip string, port int) {
-	listenAddr := net.JoinHostPort(ip, mainhelper.Itoa(port))
-	listen, err := net.Listen("tcp", listenAddr)
+	//listenAddress := net.JoinHostPort("localhost", mainhelper.Itoa(port))
+	listenAddress := net.JoinHostPort(ip, mainhelper.Itoa(port))
+	tcpListen, err := net.Listen("tcp", listenAddress)
 	if err != nil {
 		log.Fatal(natheerspretty.RGBify(255, 0, 0, "error: ", err))
 	}
-	fmt.Println(natheerspretty.RGBify(0, 255, 0, "Server is listening on: ", listenAddr))
-	defer listen.Close()
+	fmt.Println(natheerspretty.RGBify(0, 255, 0, "Server is listening on: ", listenAddress))
+	defer tcpListen.Close()
 
 	for {
-		conn, err := listen.Accept()
+		conn, err := tcpListen.Accept()
 		if err != nil {
 			log.Fatal(natheerspretty.RGBify(255, 0, 0, "error: ", err))
 		}
